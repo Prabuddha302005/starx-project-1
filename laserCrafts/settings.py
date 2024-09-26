6adeeb2e0aa07249b5b41b519c4f34352745bd41
prabuddha chunchekar <prabuddhachunchekar@gmail.com>
2024-09-27 00:43:54 +0530

    update

--- laserCrafts/settings.py
+++ laserCrafts/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tka+usu0i=j##y8m0@2ffola3(8a3q39q8p=m((oc35_&8&mil
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
